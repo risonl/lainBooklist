9fcd5debe01b146d977fd19ac430606dc44f19ae
risonl <risonlain@gmail.com>
2022-01-04 11:44:50 -0700

    set up local state management

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Card, Container, Row, Col} from 'react-bootstrap'
+import {useState} from 'react'
+
 function App() {
+  const [data, setData] = useState({
+    title: "Something",
+    author: "Someone",
+    synopsis: "Events",
+  })
+  
+  function handleSubmit(e) {
+    var bookTitle = document.getElementById('title').value
+    var bookAuthor = document.getElementById('author').value
+    var bookSynopsis = document.getElementById('synopsis').value
+    e.preventDefault();
+    setData({
+      title: bookTitle,
+      author: bookAuthor,
+      synopsis: bookSynopsis,
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <form>
+        <form onSubmit={handleSubmit}>
         <h1>Title</h1>
-        <input type = 'text' id="title" name="title" value='Title'></input>
+        <input type = 'text' id="title" name="title" defaultValue={data.title}></input>
         <h1>Author</h1>
-        <input type = 'text' id="author" name="author" value ='Author'></input>
+        <input type = 'text' id="author" name="author" defaultValue ={data.author}></input>
         <h1>Synopsis</h1>
-        <input type = 'text' id="synopsis" name="synopsis" value='Synopsis'></input>
+        <input type = 'text' id="synopsis" name="synopsis" defaultValue={data.synopsis}></input>
         <br></br>
         <Button as='input' type = 'submit'></Button>
         </form>
@@ -20,9 +39,9 @@ function App() {
       <Container>
       <Card>
         <Card.Body>
-          <Card.Title>To Kill A Mockingbird</Card.Title>
-          <Card.Text>Harper Lee</Card.Text>
-          <Card.Text>A story about a lawyer and his daughter and also race relations in the Jim Crow south.</Card.Text>
+          <Card.Title>{data.title}</Card.Title>
+          <Card.Text>{data.author}</Card.Text>
+          <Card.Text>{data.synopsis}</Card.Text>
         </Card.Body>
       </Card>
       </Container>
